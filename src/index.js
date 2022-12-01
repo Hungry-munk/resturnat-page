a25bc374ebaefdfe89004dede9679d80a53845e2
Hungry_munk <abbaszadeharsam@gmail.com>
2022-12-01 18:56:36 +1100

    added highlited tab feature

--- src/index.js
+++ src/index.js
@@ -12,18 +12,28 @@ content.appendChild(buildFooter())
 const homeBtn = document.querySelector(".homeBtn")
 const menuBtn = document.querySelector(".menuBtn")
 const contactBtn = document.querySelector(".contactBtn")
+homeBtn.classList.add("active")
 
 homeBtn.addEventListener("click", e =>{
     document.querySelector("main").remove()
     content.appendChild(buildHome())
+    homeBtn.classList.add("active")
+    menuBtn.classList.remove("active")
+    contactBtn.classList.remove("active")
 })
 
 menuBtn.addEventListener("click", e =>{
     document.querySelector("main").remove()
     content.appendChild(buildMenu())
+    menuBtn.classList.add("active")
+    homeBtn.classList.remove("active")
+    contactBtn.classList.remove("active")
 })
 
 contactBtn.addEventListener("click", e =>{
     document.querySelector("main").remove()
     content.appendChild(buildContact())
+    contactBtn.classList.add("active")
+    homeBtn.classList.remove("active")
+    menuBtn.classList.remove("active")
 })
