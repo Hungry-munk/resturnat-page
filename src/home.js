49e403b0a5c69a90eb350e4b10e2a9bc46ef15bd
Hungry_munk <abbaszadeharsam@gmail.com>
2022-12-01 17:56:43 +1100

    home styling done

--- src/home.js
+++ src/home.js
@@ -1,31 +1,34 @@
 export default function buildHome () {
-    const home = document.createElement("main")
+    const home = document.createElement("div")
+    const homeContainer = document.createElement("main")
     const image = document.createElement("img")
     const imageTxt = document.createElement('div')
     // image txt stuff
-    const headChefTxt = document.createElement("div")
+    const headChefHeader = document.createElement("div")
     const headChefName = document.createElement("div")
     const headChefPara = document.createElement("div")
 
-    headChefTxt.textContent = "HEAD CHEF"
+    headChefHeader.textContent = "HEAD CHEF"
     headChefName.textContent = "Arsam Abbasadeh"
     headChefPara.textContent = "Paragraph Text. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Sed posuere consectetur est at lobortis. Cras, dapibus ac facilisis in, meet head chef - Arsam Abbaszadeh."
 
     image.classList.add("chefImage")
-    headChefTxt.classList.add("headChefTxt")
+    imageTxt.classList.add("chefImageTxt")
+    headChefHeader.classList.add("headChefHeader")
     headChefName.classList.add("headChefName")
     headChefPara.classList.add("headChefPara")
 
     image.setAttribute("src","https://lifeandlaw.weebly.com/uploads/1/2/6/0/126058316/chef-2x_1.png")
     image.setAttribute('alt',"chef iamge")
 
-    imageTxt.appendChild(headChefTxt)
+    imageTxt.appendChild(headChefHeader)
     imageTxt.appendChild(headChefName)
     imageTxt.appendChild(headChefPara)
 
     home.appendChild(image)
     home.appendChild(imageTxt)
-    home.classList.add("main","home")
+    homeContainer.classList.add("main","home")
+    homeContainer.appendChild(home)
 
-    return home
+    return homeContainer
 }
